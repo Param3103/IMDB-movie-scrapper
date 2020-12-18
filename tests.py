a2de646d3595626d8c16b10a4a433189751cd232
Param3103 <73064464+Param3103@users.noreply.github.com>
2020-12-18 18:18:41 +0800

    Add files via upload

---
+++ tests.py
@@ -0,0 +1,29 @@
+import unittest
+import csv
+from IMDB_movies_rating_sorted import movies
+import re
+data = []
+with open('IMDBmovies.txt', 'r') as file:
+    csv_reader = csv.reader(file)
+
+    for line in csv_reader:
+        if len(line) != 0:
+            data.append(line)
+class Testing_Project(unittest.TestCase):
+    #tests if all movies have been deposited into csv file
+    def test_if_deposited(self):
+        for movie in movies:
+            for i in movie:
+                movie[movie.index(i)] = re.sub('\n', '', i)
+            movie[0] = movie[0][14:-1] + ')'
+            self.assertIn(movie, data)
+    # test if each movie value is a pair
+    def test_is_pair(self):
+        for movie in data[0: len(data)]:
+            self.assertEqual(len(movie), 2)
+    #tests if movies have been sorted according to rating
+    def test_if_sorted(self):
+        for movie in data[0: len(data) - 1]:
+            self.assertTrue(movie[1] <= data[data.index(movie) + 1][1])
+if __name__ == '__main__':
+    unittest.main()
