1d800163ad1953551926cea2dadf6d43886fa9b0
Param3103 <param.mehrotra@gmail.com>
2020-12-23 22:32:39 +0800

    done as of 22/12/2020

--- sort_by_given_criterion.py
+++ sort_by_given_criterion.py
@@ -46,7 +46,7 @@ class ManageData:
         for movie in movies:
             while (movie.released_year.split())[0] == ' ':
                 movie.released_year = movie.released_year[1:-1]
-
+        return(movies)
     def write_to_csv(movies, filename):
         with open(filename, 'w') as file:
             csv_writer = csv.writer(file)
