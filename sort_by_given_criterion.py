77699a46bb0d53da88dd1fb4c72482f39cce1404
Param3103 <param.mehrotra@gmail.com>
2020-12-23 22:30:50 +0800

    done as of 22/12/2020

--- sort_by_given_criterion.py
+++ sort_by_given_criterion.py
@@ -44,8 +44,8 @@ class ManageData:
         return(movies)
     def remove_start_space(movies):
         for movie in movies:
-            while (movie[0].split())[0] == ' ':
-                movie[0] = movie[0][1:-1]
+            while (movie.released_name.split())[0] == ' ':
+                movie.released_name = movie.released_name[1:-1]
 
     def write_to_csv(movies, filename):
         with open(filename, 'w') as file:
