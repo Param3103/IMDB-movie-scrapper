3c7a8409b084aa2faf9c8eda276e0e2714bb36ef
Param3103 <param.mehrotra@gmail.com>
2020-12-24 12:06:35 +0800

    done as of 22/12/2020

--- sort_by_given_criterion.py
+++ sort_by_given_criterion.py
@@ -67,5 +67,6 @@ for i in ExtractData.extract_data(website2):
 movies.sort(key=ManageData.sorts)
 movies = ManageData.remove_line_break(movies)
 movies = ManageData.remove_start_space(movies)
-print(movies)
+for movie in movies:
+    print(movie.name)
 ManageData.write_to_csv(movies, 'IMDBmovies.csv')
