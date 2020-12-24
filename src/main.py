5ea88d43c23333ff4996a1f3ca7904231ed702df
Param3103 <param.mehrotra@gmail.com>
2020-12-24 11:46:42 +0800

    done as of 22/12/2020

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
 from src.extractdata import ExtractData
-hindi_movie_url = "https://www.imdb.com/india/top-rated-indian-movies/?pf_rd_m=A2FGELUUNOQJNL&pf_r\
-                  d_p=4da9d9a5-d299-43f2-9c53-f0efa18182cd&pf_rd_r=F22A2RC934X0Q4NDWHBA&pf_rd_s=right\
-                  -4&pf_rd_t=15506&pf_rd_i=top&ref_=chttp_ql_7"
+hindi_movie_url = "https://www.imdb.com/india/top-rated-indian-movies/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=4da9d9a5-d299-43f2-9c53-f0efa18182cd&pf_rd_r=F22A2RC934X0Q4NDWHBA&pf_rd_s=right -4&pf_rd_t=15506&pf_rd_i=top&ref_=chttp_ql_7"
 english_movie_url = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
 class Main:
     def process(self):
