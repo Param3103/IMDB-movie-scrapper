1a6089f5e0df369f7778304fb7e7ff9ba2eb0cb9
Param3103 <param.mehrotra@gmail.com>
2020-12-24 12:42:35 +0800

    -

--- src/main.py
+++ src/main.py
@@ -14,7 +14,6 @@ class Main:
         movies.sort(key=ManageData.sorts)
         movies = ManageData.remove_line_break(movies)
         movies = ManageData.remove_start_space(movies)
-        print(movies)
         for movie in movies:
             print('ok')
             print(movie.name)
