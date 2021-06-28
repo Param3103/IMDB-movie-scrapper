fba80dcef740aff594f407fb42a96677c569b717
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-06-28 16:40:45 +0800

    Remove ridiculously long extraction procedure from extract_data.py

--- src/extract_data.py
+++ src/extract_data.py
@@ -1,25 +1,10 @@
 from urllib.request import urlopen
-import bs4
 from bs4 import BeautifulSoup as soup
 from movie import Movie
 
-class ExtractData:
-
-    def open_page(url):
-        uClient = urlopen(url)
-        page_html = uClient.read()
-        uClient.close()
-        return(page_html)
-
-    def extract_data(page_html):
-        movies = []
-        page_soup = soup(page_html, "html.parser")
-        containers = page_soup.findAll("td", {"class": "titleColumn"})
-        ratings = page_soup.findAll("td", {"class": "ratingColumn imdbRating"})
-        for container in containers:
-            name = container.text[2: -6]
-            year = container.text[-6: -2]
-            movie = Movie(year, name, ratings[containers.index(container)].text)
-            movies.append(movie)
-        return(movies)
-ExtractData
+def extract_data(url):
+    with urlopen(url) as uClient: page_html = uClient.read()
+    page_soup = soup(page_html, "html.parser")
+    containers = page_soup.findAll("td", {"class": "titleColumn"})
+    ratings = page_soup.findAll("td", {"class": "ratingColumn imdbRating"})
+    return [Movie.fromSoup(containers[i], ratings[i]) for i in range(min(len(containers), len(ratings)))]
