524c2186a32c4433f378e86bf8d1a819957a8673
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-06-28 16:42:07 +0800

    Shorten main process in main.py

--- src/main.py
+++ src/main.py
@@ -1,19 +1,14 @@
-from extract_data import ExtractData
-from manage_data import ManageData
+from extract_data import extract_data
+from manage_data import write_to_csv
+
 english_movie_url = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
 hindi_movie_url = "https://www.imdb.com/india/top-rated-indian-movies/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=4da9d9a5-d299-43f2-9c53-f0efa18182cd&pf_rd_r=F22A2RC934X0Q4NDWHBA&pf_rd_s=right-4&pf_rd_t=15506&pf_rd_i=top&ref_=chttp_ql_7"
-class Main:
-    def process(self):
-        movies = []
-        website1 = ExtractData.open_page(hindi_movie_url)
-        website2 = ExtractData.open_page(english_movie_url)
-        for i in ExtractData.extract_data(website1):
-            movies.append(i)
-        for i in ExtractData.extract_data(website2):
-            movies.append(i)
-        movies.sort(key=ManageData.sorts)
-        movies = ManageData.remove_line_break(movies)
-        movies = ManageData.remove_start_space(movies)
-        ManageData.write_to_csv(movies, 'IMDBmovies.csv')
 
-Main.process(None)
+def main():
+    movies = extract_data(hindi_movie_url) + extract_data(english_movie_url)
+    movies.sort(key=lambda movie: movie.released_year)
+    write_to_csv(movies, 'IMDBmovies.csv')
+
+
+if __name__ == "__main__":
+    main()
