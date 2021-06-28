6c30f2c6199b736a3750638399d6775fe834bb46
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-06-28 16:40:14 +0800

    Add some configurational features to movie.py

--- src/movie.py
+++ src/movie.py
@@ -1,6 +1,12 @@
 class Movie:
     def __init__(self, released_year, name, rating):
-        self.released_year = released_year
-        self.name = name
-        self.rating = rating
-Movie
+        self.released_year = released_year.replace("\n", "")
+        self.name = name.replace("\n", "")[13:-1].strip()
+        self.rating = rating.replace("\n", "")
+
+    def __str__(self):
+        return ",".join([self.released_year, self.name, self.rating])
+
+    @classmethod
+    def fromSoup(cls, container, rating):
+        return cls(container.text[-6: -2], container.text[2: -6], rating.text)
